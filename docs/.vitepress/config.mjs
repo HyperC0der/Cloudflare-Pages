1ab416202760e6d4357c8359e6012fa35c141029
liang <liangweisheng>
2024-01-04 15:16:55 +0800

    fix: 添加vitepress

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "HyperC0der博客",
+  description: "天天抄代码，一天不抄就手痒",
+  base:'/hyperC0der/',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
